6d616bb525b99488cf434140bd55570a15e1ffe2
Kent Hansen <kenthansen98@gmail.com>
2021-12-01 15:44:13 -0600

    added basic game mechanics

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -12,28 +12,54 @@ interface Props {
         startCities: City[];
         endCities: City[];
     };
-    geojson: {
+    tripTypes: string[];
+    geojsons: {
         type: string;
         properties: {};
         geometry: {
             type: string;
             coordinates: any;
         };
-    };
+    }[];
+    durations: number[];
 }
 
 const DynamicMap = dynamic(() => import("../../components/Map"), {
     ssr: false,
 });
 
-const Game: React.FC<Props> = ({ game, geojson }) => {
+const Game: React.FC<Props> = ({ game, tripTypes, geojsons, durations }) => {
     const { data: session, status } = useSession();
     const loading = status === "loading";
     const [round, setRound] = useState(0);
-    const midpoint = [
+    const [midpoint, setMidpoint] = useState([
         (game.startCities[round].lat + game.endCities[round].lat) / 2,
         (game.startCities[round].long + game.endCities[round].long) / 2,
-    ];
+    ]);
+    const [enteringGuess, setEnteringGuess] = useState(false);
+    const [hours, setHours] = useState<number | undefined>();
+    const [minutes, setMinutes] = useState<number | undefined>();
+    const [guessed, setGuessed] = useState(false);
+    const [score, setScore] = useState(0);
+    const [overallScore, setOverallScore] = useState(0);
+
+    const onEnterGuess = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        const guess = hours + minutes / 60;
+        const roundScore = Math.ceil(
+            100 - Math.abs(guess / durations[round] - 1) * 100
+        );
+        setScore(roundScore);
+        setOverallScore(overallScore + roundScore);
+        setGuessed(true);
+        setEnteringGuess(false);
+    };
+
+    const onNextRound = () => {
+        setRound(round + 1);
+        setGuessed(false);
+        setEnteringGuess(false);
+    };
 
     if (loading) {
         return (
@@ -67,24 +93,107 @@ const Game: React.FC<Props> = ({ game, geojson }) => {
     }
 
     return (
-        <div className="absolute">
-            <div className="h-screen w-screen -z-1 absolute">
-                <DynamicMap lat={midpoint[0]} long={midpoint[1]} route={geojson} />
+        <div className="absolute flex flex-col">
+            <div className="bg-gray-600 h-screen w-screen z-0 absolute">
+                <DynamicMap lat={midpoint[0]} long={midpoint[1]} route={geojsons[round]} />
             </div>
-            <div className="w-screen flex flex-row justify-between">
-                <div className="z-10 m-10 p-5 bg-white rounded shadow-lg flex flex-col">
+            <div className="w-screen flex flex-row justify-between h-1/4">
+                <div className="m-10 p-5 bg-white rounded shadow-lg flex flex-col h-full z-10">
                     <h1 className="font-bold text-xl">Round {round + 1}:</h1>
+                    <h3 className="font-semibold text-lg text-gray-500">
+                        {tripTypes[round]}
+                    </h3>
                     <p>
-                        {game.startCities[0].name} {"-->"}{" "}
-                        {game.endCities[0].name}
+                        {game.startCities[round].name} {"-->"}{" "}
+                        {game.endCities[round].name}
                     </p>
                 </div>
-                <div className="m-10 flex">
-                    <button className="bg-gradient-to-r from-blue-800 to-green-800 text-lg my-auto font-bold p-2 text-white rounded-md shadow-sm transition hover:scale-110">
-                        Enter Guess
-                    </button>
+                <div className="flex">
+                    {!guessed && !enteringGuess && (
+                        <button
+                            className="bg-gradient-to-r from-blue-800 to-green-800 text-lg my-auto mx-10 font-bold p-2 text-white rounded-md shadow-sm transition hover:scale-110 z-10"
+                            onClick={() => setEnteringGuess(true)}
+                        >
+                            Enter Guess
+                        </button>
+                    )}
+                    {enteringGuess && (
+                        <div className="m-10 p-5 flex flex-col bg-white shadow-lg rounded z-10">
+                            <form className="flex flex-col">
+                                <div className="flex flex-row justify-between">
+                                    <h2 className="text-lg font-medium self-center">
+                                        Enter hours:{" "}
+                                    </h2>
+                                    <input
+                                        className="form-input ring-1 ring-gray-600 focus:ring-2 focus:ring-blue-800 rounded-sm m-3"
+                                        type="number"
+                                        onChange={(event) =>
+                                            setHours(Number(event.target.value))
+                                        }
+                                    />
+                                </div>
+                                <div className="flex flex-row justify-between">
+                                    <h2 className="text-lg font-medium self-center">
+                                        Enter minutes:{" "}
+                                    </h2>
+                                    <input
+                                        className="form-input ring-1 ring-gray-600 focus:ring-2 focus:ring-blue-800 rounded-sm m-3"
+                                        type="number"
+                                        onChange={(event) =>
+                                            setMinutes(
+                                                Number(event.target.value)
+                                            )
+                                        }
+                                    />
+                                </div>
+                                <button
+                                    className="bg-gradient-to-r from-blue-800 to-green-800 text-md my-2 mx-auto font-semibold p-2 text-white rounded-md shadow-sm transition hover:scale-105"
+                                    onClick={onEnterGuess}
+                                >
+                                    Submit
+                                </button>
+                            </form>
+                        </div>
+                    )}
                 </div>
             </div>
+            {guessed && (
+                <div className="w-1/4 top-1/2 left-1/2 bg-white m-auto flex flex-col items-center rounded shadow-lg p-3 z-10">
+                    <h2 className="text-lg font-bold m-3">Round Results</h2>
+                    <p className="text-md font-semibold mx-2 text-gray-600">
+                        You Guessed:{" "}
+                    </p>
+                    {hours}h {minutes}mins
+                    <p className="text-md font-semibold mx-2 text-gray-600">
+                        Actual Duration:{" "}
+                    </p>
+                    {Math.floor(durations[round])}h{" "}
+                    {Math.floor(
+                        (durations[round] - Math.floor(durations[round])) * 60
+                    )}
+                    mins
+                    <div className="flex flex-row my-3">
+                        <p className="text-md font-semibold mx-2 text-gray-600">
+                            Score:{" "}
+                        </p>
+                        {score}
+                    </div>
+                    <div className="flex flex-row mb-3">
+                        <p className="text-md font-semibold mx-2 text-gray-600">
+                            Overall Score:{" "}
+                        </p>
+                        {overallScore}
+                    </div>
+                    {round < game.numRounds - 1 && (
+                        <button
+                            className="bg-gradient-to-r from-blue-800 to-green-800 text-md my-2 mx-auto font-semibold p-2 text-white rounded-md shadow-sm transition hover:scale-105"
+                            onClick={onNextRound}
+                        >
+                            Next Round
+                        </button>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
@@ -112,26 +221,38 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
         },
     });
 
-    const curTripType: String = `mapbox/${
-        game.tripTypes[Math.floor(Math.random() * game.tripTypes.length)]
-    }`;
-    const directionsEnpoint = `https://api.mapbox.com/directions/v5/${curTripType}/${game.startCities[0].long},${game.startCities[0].lat};${game.endCities[0].long},${game.endCities[0].lat}?geometries=geojson&access_token=${process.env.NEXT_PUBLIC_MAPS_API}`;
-    const directionRes = await fetch(directionsEnpoint);
-    const dirJson = await directionRes.json();
-    const route = dirJson.routes[0].geometry.coordinates;
-    const geojson = {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'LineString',
-          coordinates: route
-        }
-      };
+    const tripTypes: string[] = [];
+    const geojsons = [];
+    let durations: number[] = [];
+
+    for (let i = 0; i < game.startCities.length; i++) {
+        const curTripType =
+            game.tripTypes[Math.floor(Math.random() * game.tripTypes.length)];
+        const curTripTypeString: string = `mapbox/${curTripType}`;
+        tripTypes.push(curTripType);
+        const directionsEnpoint = `https://api.mapbox.com/directions/v5/${curTripTypeString}/${game.startCities[i].long},${game.startCities[i].lat};${game.endCities[i].long},${game.endCities[i].lat}?geometries=geojson&access_token=${process.env.NEXT_PUBLIC_MAPS_API}`;
+        const directionRes = await fetch(directionsEnpoint);
+        const dirJson = await directionRes.json();
+        const duration = dirJson.routes[0].duration / 3600;
+        durations.push(duration);
+        const route = dirJson.routes[0].geometry.coordinates;
+        const geojson = {
+            type: "Feature",
+            properties: {},
+            geometry: {
+                type: "LineString",
+                coordinates: route,
+            },
+        };
+        geojsons.push(geojson);
+    }
 
     return {
         props: {
             game,
-            geojson,
+            tripTypes,
+            geojsons,
+            durations,
         },
     };
 };
